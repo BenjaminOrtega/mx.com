199c09173b1c71127f9ce7071e1b917e98ea8f3f
Benjamín Ortega <benjaminortega@localhost.localdomain>
2020-12-25 23:45:36 -0600

    conexion  y consulta a mysql

--- src/main/java/proyectoBD/mx/com/App.java
+++ src/main/java/proyectoBD/mx/com/App.java
@@ -1,5 +1,7 @@
 package proyectoBD.mx.com;
 
+import java.sql.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello como estas" );
+        String  url = "jdbc:mysql://localhost:3306/Test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        try {
+			Connection conexion = DriverManager.getConnection(url,"root","");
+			Statement intriduccion = conexion.createStatement();
+			String sql = "SELECT id_persona,nombre,apellido,email,telefono FROM Persona";
+			ResultSet  resultado = intriduccion.executeQuery(sql);
+			
+			while (resultado.next()) {
+				
+				System.out.println("**************** " + resultado.getInt("id_persona"));
+				System.out.println("Nombre: " + resultado.getString("nombre"));
+				System.out.println("Apellido: " + resultado.getString("apellido"));
+				System.out.println("Email: " + resultado.getString("email"));
+				System.out.println("Telefono: " + resultado.getString("telefono"));
+			}
+			resultado.close();
+			intriduccion.close();
+			conexion.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace(System.out);
+		}
     }
 }
