906aec0f87a45b19aff0610c2b8337802eb51ec0
Benjamin Ortega <eduardobenjamin.ortega@gmail.com>
2021-01-24 19:13:12 -0600

    se agregaron pool de colecciones

--- src/main/java/datos/mx/com/Conexion.java
+++ src/main/java/datos/mx/com/Conexion.java
@@ -2,13 +2,27 @@ package datos.mx.com;
 
 import java.sql.*;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class Conexion {
 	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/Test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
 	private static final String JDBC_USER = "root";
 	private static final String JDBC_PASS = "";
 	
+	public static DataSource getDataSource () {
+		BasicDataSource basicDataSource = new BasicDataSource();
+		basicDataSource.setUrl(JDBC_URL);
+		basicDataSource.setUsername(JDBC_USER);
+		basicDataSource.setPassword(JDBC_PASS);
+		//definimos el numero inical de conexiones
+		basicDataSource.setInitialSize(5);
+		return basicDataSource;
+	}	
+	
 	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(JDBC_URL,JDBC_USER,JDBC_PASS);
+		return getDataSource().getConnection();
 	}
 	
 	public static void close(ResultSet rs) throws SQLException {
